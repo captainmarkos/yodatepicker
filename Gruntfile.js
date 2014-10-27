08c6ca7e611cf6e9d7e40003ff2933ad97353225
Mark Brettin <mark@jackrabbitsystems.com>
2014-10-27 11:31:04 -0400

    Adds karma and jasmine.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,20 +11,30 @@ module.exports = function(grunt) {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
             all: {
-                src: ['app/js/{,*/}*.js']
+                src: ['Gruntfile.js',
+                      'app/js/{,*/}*.js']
             }
         },
         nodeunit: {
             files: ['test/**/*_test.js']
         },
+        // Test settings
+        karma: {
+            unit: {
+                configFile: 'test/karma.conf.js',
+                singleRun: true
+            }
+        },
+
         watch: {
             gruntfile: {
                 files: '<%= jshint.gruntfile.src %>',
                 tasks: ['jshint:gruntfile']
+            },
+            jsTest: {
+                files: ['test/unit/*.js'],
+                tasks: ['jshint:all', 'karma']
             },
             lib_test: {
                 files: '<%= jshint.lib_test.src %>',
@@ -37,11 +47,23 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
 
     // Default task.
     grunt.registerTask('default', ['jshint', 'nodeunit']);
 
-    grunt.registerTask('test', [
-        'jshint:all'
-    ]);
+    grunt.registerTask('test', 'runs jshint and karma',
+        function(test) {
+            if(test === 'unit') {
+                grunt.task.run([
+                    'karma'
+                ]);
+            } else {
+                grunt.task.run([
+                    'jshint:all',
+                    'karma'
+                ]);
+            }
+        }
+    );
 };
---
+++ test/unit/yodatepicker_spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('yodatepicker', function() {
+
+    it('foo bar', function() {
+        var checkin_datepicker = yodatepicker({});
+        console.log('checkin_datepicker:');
+        console.log(checkin_datepicker);
+
+        expect(checkin_datepicker).toBeDefined();
+    });
+});
